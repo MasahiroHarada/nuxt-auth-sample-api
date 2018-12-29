806dab8944fb91ef42b485906870b73541f1e519
Masahiro Harada <masahiro.harada@ari-jp.com>
2018-12-29 23:44:32 +0900

    Ajaxのみを許可するミドルウェアを追加

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\AjaxOnly;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -48,6 +49,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\AjaxOnly::class,
             \Barryvdh\Cors\HandleCors::class,
         ],
     ];
